1cda1bb96f2b2f34ffe796edf0bafc40f8f31cc8
Kemneti <98813392+Kemneti@users.noreply.github.com>
2022-08-31 20:28:26 +0100

    py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,10 +2,3 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = [[x ** 2 for x in row] for row in matrix]
     return new_matrix
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-St
